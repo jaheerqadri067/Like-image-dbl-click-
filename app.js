1e2c06a5c05ebd45b728f49df3ebaad49d785135
jaheershaikh <jaheershaikh067@gmail.com>
2024-03-16 16:09:10 +0530

    like btn

---
+++ app.js
@@ -0,0 +1,24 @@
+//INSTAGARAM LIKE FEATURE ACTIVITY
+let image = document.querySelector(".like-photo");
+let heart = document.querySelector("#like");
+
+image.addEventListener("dblclick", () => {
+    heart.style.opacity = 1
+    heart.style.transform = "scale(3.4)";
+
+    setTimeout(() => {
+        heart.style.transform = "scale(3)";
+    }, 300)
+
+    setTimeout(() => {
+        heart.style.transform = "scale(3.4)";
+    }, 450)
+
+    setTimeout(() => {
+        heart.style.opacity = 0;
+    }, 900);
+
+    setTimeout(() => {
+        heart.style.transform = "scale(0)";
+    }, 1000);
+});
